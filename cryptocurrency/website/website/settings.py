012b12095fe62c7f83bdf5871b8a09b162ae8b8a
okoriek <66570436+okoriek@users.noreply.github.com>
2024-01-22 00:48:13 +0100

    updated error

--- cryptocurrency/website/website/settings.py
+++ cryptocurrency/website/website/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import os
 from zoneinfo import ZoneInfo
 import os
-from dotUenv import load_dotenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
